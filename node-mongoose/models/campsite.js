13c6950350544288b2579b5cb1dd4cc0cf21fcf6
Chang Lee <chang.bok.lee88@gmail.com>
2022-05-19 23:03:18 -0400

    Mongoose Part 3

--- node-mongoose/models/campsite.js
+++ node-mongoose/models/campsite.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +27,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
